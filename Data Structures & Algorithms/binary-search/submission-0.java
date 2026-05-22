c01c9b6bd3a55d744268e6503a01c93ddc2ab3ac
Kaushikee Bhatt <kaushikeebhatt4@gmail.com>
2026-05-23 00:52:50 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int p1=0;
+        int p2=nums.length-1;
+        while(p1<=p2){
+            int mid = (p1+p2)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if(nums[mid]> target){
+                p2=mid-1;
+            }
+            else{
+                p1=mid+1;
+            }
+        }
+        return -1;
+
+    }
+}
